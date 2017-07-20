01a8dc0fa95f1d05c8c163757eb44d1ab13d624c
Joshua Rosenberg <jrosen@msu.edu>
2017-07-20 19:45:43 -0400

    Update function.R

--- R/function.R
+++ R/function.R
@@ -1,19 +1,4 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Cmd + Shift + B'
-#   Check Package:             'Cmd + Shift + E'
-#   Test Package:              'Cmd + Shift + T'
-
-t_tester <- function(dv, fac, df) {
+t_test <- function(dv, fac, df) {
 
     dv_q <- as.character(substitute(dv))
     fac_q <- as.character(substitute(fac))
